294e2e0a778d7d6ffdf882b8fa5e12fc804df6e2
Janie L <janie@Janies-MacBook-Pro.local>
2016-07-27 00:08:58 +0800

    change staging server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,1 @@
-server 'ec2-52-40-79-131.us-west-2.compute.amazonaws.com', user: 'deployer', roles: %w{web app db}
+server 'ec2-52-26-107-7.us-west-2.compute.amazonaws.com', user: 'deployer', roles: %w{web app db}
