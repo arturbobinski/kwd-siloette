8d4d4b7fdf5d9ad56080b131a462d1e72f505405
Janie L <janie@Janies-MacBook-Pro.local>
2016-08-27 04:00:46 +0800

    fix S3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
       provider: 'AWS',
       aws_access_key_id: Figaro.env.aws_access_key_id,
       aws_secret_access_key: Figaro.env.aws_secret_access_key,
-      region: 'us-east-1',
+      region: 'us-west-2',
     }
 
     config.fog_directory  = Figaro.env.aws_bucket
