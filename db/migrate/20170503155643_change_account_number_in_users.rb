da0353b36741c26a2ddca5df2f0f555ee141092f
Artur Bobinski <artur@kentonwebdesign.com>
2017-05-03 16:05:49 +0000

    made database changes for account number from integer to string

--- db/migrate/20170503155643_change_account_number_in_users.rb
+++ db/migrate/20170503155643_change_account_number_in_users.rb
@@ -1,11 +1,11 @@
 class ChangeAccountNumberInUsers < ActiveRecord::Migration
   def change
     def up
-      remove_column :users, :account_number, :integer
+      add_column :users, :account_number, :integer
     end
 
     def down
-      add_column :users, :account_number, :string
+      remove_column :users, :account_number, :string
     end
   end
 end
