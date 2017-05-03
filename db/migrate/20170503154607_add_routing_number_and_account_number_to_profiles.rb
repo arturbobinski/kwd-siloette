306d055b8cb019e4de0f7263c07dfe92db22fb85
Artur Bobinski <artur@kentonwebdesign.com>
2017-05-03 15:51:58 +0000

    added new table for routing number and account number into db

--- db/migrate/20170503154607_add_routing_number_and_account_number_to_profiles.rb
+++ db/migrate/20170503154607_add_routing_number_and_account_number_to_profiles.rb
@@ -2,7 +2,7 @@ class AddRoutingNumberAndAccountNumberToProfiles < ActiveRecord::Migration
   def change
     add_column :users, :routing_number, :string
     add_index :users, :routing_number
-    add_column :users, :account_number, :integer
+    add_column :users, :account_number, :string
     add_index :users, :account_number
   end
 end
