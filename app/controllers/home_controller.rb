66f732a2b0ce402771db13f1eb772f9774acb7e8
Artur Bobinski <artur@kentonwebdesign.com>
2017-05-11 15:30:48 +0000

    pluralize top services

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
 
   def index
     @q = Service.open.active.search(params[:q])
-    @top_services = Service.top_rated.limit(4).includes(:category, :primary_image, performers: :profile).pluralize(1)
+    @top_services = Service.top_rated.limit(4).includes(:category, :primary_image, performers: :profile)
   end
 
   def work_with_us
