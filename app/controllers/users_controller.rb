4aa57bb2b2c5ffea0e44845b25e7d2a5478ebdb8
Artur Bobinski <artur@kentonwebdesign.com>
2017-06-05 15:14:06 +0000

    user path on sign in change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,34 @@ class UsersController < ApplicationController
     build_resources
   end
 
- def update
+ #def update
+ #   if @user.update(user_params)
+#      if @user.dancer? 
+#        redirect_to edit_user_path(@user), notice: t('.notice')
+#      end
+#    else
+#      if @user.dancer?
+#        # format.html { render :edit }
+#      end
+#    end
+#  end
+
+  def update
     if @user.update(user_params)
-      if @user.dancer? 
+      if @user.dancer?
+        UserMailer.user_verification_email(@user).deliver_later
+        UserMailer.user_verifying_email(@user).deliver_later
+        redirect_to :back
+      elsif path = next_path(@user)
+        redirect_to path
+      else
         redirect_to edit_user_path(@user), notice: t('.notice')
       end
     else
-      if @user.dancer?
-        # format.html { render :edit }
+      if !@user.verified?
+        render 'users/apply'
+      else
+        render :edit
       end
     end
   end
